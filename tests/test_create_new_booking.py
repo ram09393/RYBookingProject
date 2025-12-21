cca9f251bbdfdb289b281938a9239166709e801e
RAMISK <rkomiakov@mail.ru>
2025-12-22 01:54:44 +0300

    - убрал лишние моки из тестов - переделал тест def test_create_booking_timeout

--- tests/test_create_new_booking.py
+++ tests/test_create_new_booking.py
@@ -1,90 +1,81 @@
 import allure
 import pytest
 import requests
+from pydantic import ValidationError
+from self import self
+from core.clients.api_client import ApiClient
+from urllib3.exceptions import InsecureRequestWarning
 
 from core.clients.endpoints import Endpoints
+from core.models.booking import BookingResponse
 
 
-# Каждая функция проверяет разные сценарии:
-# - успешное создание брони
-# - ошибка из-за отсутствия полей
-# - ошибка с сервером (некорректные данные)
+class Api_Client:
+    @pytest.fixture
+    def create_booking(self, booking_data):
+        response = requests.post("https://restful-booker.herokuapp.com/booking", json=booking_data, verify=False)
+        return response
 
 
+BOOKING_URL = "https://restful-booker.herokuapp.com/booking"
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+
+# - Успешное создание бронирования
 @allure.feature('Test Create Booking')
 @allure.story('Test successful booking creation')
-def test_create_booking_success(api_client, mocker, mock_response=None):
-    mock_response = mocker.Mock()
-    mock_response.status_code = 200
-    mock_response.json.return_value = {"booking_id": 2}
-    mocker.patch.object(api_client.session, 'post', return_value=mock_response)
+def test_create_booking_success():
+    booking_data = {
+        "firstname": "Jack",
+        "lastname": "Rain",
+        "totalprice": 153,
+        "depositpaid": True,
+        "bookingdates": {
+            "checkin": "2025-12-21",
+            "checkout": "2025-12-30"
+        },
+        "additionalneeds": "Lunch"
+    }
 
-    response = api_client.create_booking(
-        url="https://restful-booker.herokuapp.com/",
+    # Отправляем POST-запрос на создание бронирования
+    response = requests.post(BOOKING_URL, json=booking_data, verify=False)
 
-        booking_data={
-            "firstname": "John",
-            "lastname": "Doe",
-            "totalprice": 133,
-            "depositpaid": True,
-            "bookingdates": {
-                "checkin": "2025-10-01",
-                "checkout": "2025-10-10"
-            },
-            "additionalneeds": "Lunch"
-        })
+    # Проверяем статус-код ответа
+    assert response.status_code == 200, f"Expected status code 200 but got {response.status_code}"
 
-    assert response['booking_id'] == 2, f"Expected booking_id 1 but got {response['booking_id']}"
+    # Получаем JSON-ответ и проверяем наличие booking_id
+    booking_data = response.json()
+    assert 'bookingid' in booking_data, "Response not contain booking_id field."
 
+
+# - Ошибка из-за отсутствия обязательных полей
 
 @allure.feature('Test Create Booking')
 @allure.story('Test booking creation with missing fields')
-def test_create_booking_missing_fields(api_client, mocker, mock_response=None):
-    mock_response = mocker.Mock()
-    mock_response.status_code = 404
-    mocker.patch.object(api_client.session, 'post', return_value=mock_response)
+def test_create_booking_missing_fields():
+    booking_data = {
+        "firstname": "Jimmy",
+        "lastname": "Dimple",
+        "totalprice": 100,
 
-    with pytest.raises(AssertionError, match="Expected status 200 but got 404"):
-        api_client.create_booking({
-            "firstname": "Jimmy",
-            "lastname": "Dimple",
-            "totalprice": 100,
-        })
+    }
 
+    # Запрашиваем бронирование с недостаточными полями
+    response = requests.post(BOOKING_URL, json=booking_data, verify=False)
+    # Проверяем, что сервер вернул статус 400
+    assert response.status_code == 500, "Ожидался статус-код 500"
 
-@allure.feature('Test Create Booking')
-@allure.story('Test server error during booking creation')
-def test_create_booking_server_error(api_client, mocker, mock_response=None, apiclient=None):
-    mock_response = mocker.Mock()
-    mock_response.status_code = 500
-    mocker.patch.object(api_client.session, 'post', return_value=mock_response)
-
-    with pytest.raises(AssertionError, match="Expected status 200 but got 500"):
-        api_client.create_booking({
-            "firstname": "John",
-            "lastname": "Doe",
-            "totalprice": 132,
-            "depositpaid": True,
-            "bookingdates": {
-                "checkin": "2025-10-01",
-                "checkout": "2025-10-10"
-            }
-        })
+    # Проверяем, что тело ответа содержит правильное сообщение об ошибке
+    response_text = response.text.strip()
+    expected_message = "Internal Server Error"
+    assert expected_message in response_text, f"Ожидалась строка '{expected_message}' в тексте ответа."
 
 
-@allure.feature('Test Create Booking')
-@allure.story('Test timeout during booking creation')
+# Проверка поведения метода в случае, если сервер вернул таймаут
 def test_create_booking_timeout(api_client, mocker):
+    # Меняем  post-метод на метод, для получения Timeout
     mocker.patch.object(api_client.session, 'post', side_effect=requests.Timeout)
 
+    # Ожидаем исключение Timeout
     with pytest.raises(requests.Timeout):
-        api_client.create_booking({
-            "firstname": "John",
-            "lastname": "Doe",
-            "totalprice": 111,
-            "depositpaid": False,
-            "bookingdates": {
-                "checkin": "2026-13-02",
-                "checkout": "2026-13-10"
-            }
-        })
+        api_client.create_booking(BOOKING_URL, {})
